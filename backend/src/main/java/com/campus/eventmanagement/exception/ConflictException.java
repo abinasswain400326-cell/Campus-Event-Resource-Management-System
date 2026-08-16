03bfd065eb1cc43d5eda3daa4b62fbb716b2293f
abinasswain400326-cell <abinasswain400326@gmail.com>
2026-08-16 09:15:08 +0530

    Delete backend directory

--- backend/src/main/java/com/campus/eventmanagement/exception/ConflictException.java
+++
@@ -1,8 +0,0 @@
-package com.campus.eventmanagement.exception;
-
-// Thrown when a resource booking would overlap an existing one, or a duplicate RSVP is attempted.
-public class ConflictException extends RuntimeException {
-    public ConflictException(String message) {
-        super(message);
-    }
-}

76950cde90a436e9060ee11ed6c91ee7f6b87869
abinasswain400326-cell <abinasswain400326@gmail.com>
2026-08-16 09:50:25 +0530

    Delete frontend directory

--- frontend/src/app/models/user.model.ts
+++
@@ -1,9 +0,0 @@
-export type Role = 'ADMIN' | 'ORGANIZER' | 'ATTENDEE';
-
-export interface AuthResponse {
-  token: string;
-  userId: number;
-  fullName: string;
-  email: string;
-  role: Role;
-}

a8b91fb85066ddfb4e62108698fc31b33fda4e44
abinasswain400326-cell <abinasswain400326@gmail.com>
2026-08-16 09:49:54 +0530

    Delete backend directory

--- backend/src/main/java/com/campus/eventmanagement/entity/Role.java
+++
@@ -1,7 +0,0 @@
-package com.campus.eventmanagement.entity;
-
-public enum Role {
-    ADMIN,      // full control: manage users, all events, all resources
-    ORGANIZER,  // can create/manage their own events and book resources
-    ATTENDEE    // can browse events and RSVP
-}
--- backend/src/main/java/com/campus/eventmanagement/repository/ResourceRepository.java
+++
@@ -1,7 +0,0 @@
-package com.campus.eventmanagement.repository;
-
-import com.campus.eventmanagement.entity.Resource;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ResourceRepository extends JpaRepository<Resource, Long> {
-}
